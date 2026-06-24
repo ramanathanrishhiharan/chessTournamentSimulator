f25fdd6823bb2e44b280d38c0cba0f8677ee1534
ramanathanrishiharan <ramanathanrishiharan2003@gmail.com>
2026-06-24 18:57:27 +0530

    adding a feature to user input

--- src/main/java/com/rishiharan/chess/Main.java
+++ src/main/java/com/rishiharan/chess/Main.java
@@ -2,21 +2,40 @@ package com.rishiharan.chess;
 
 import com.rishiharan.chess.TournamentManager;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         TournamentManager tm = new TournamentManager();
+        Scanner input = new Scanner(System.in);
 
-        tm.addPlayer(new Player("Alice", 1800));
-        tm.addPlayer(new Player("Bob", 1700));
-        tm.addPlayer(new Player("Charlie", 1600));
-        tm.addPlayer(new Player("David", 1500));
-        tm.addPlayer(new Player("Emma", 1750));
-        tm.addPlayer(new Player("Frank", 1650));
-        tm.addPlayer(new Player("George", 1550));
-        tm.addPlayer(new Player("Henry", 1450));
+        System.out.println("Please enter the number of players are participated in the tournament : ");
+        int n = input.nextInt();
+        input.nextLine();
+
+        if (n<2){
+            System.out.println("tournament need least 2 players");
+            return;
+        }
+
+        for(int i=0; i<n; i++) {
+            System.out.println("\nPlayer " + (i + 1) + ":");
+
+            System.out.print("Name: ");
+            String name = input.nextLine();
+
+            System.out.print("ELo rating :");
+            int elo = input.nextInt();
+            input.nextLine();
+            tm.addPlayer(new Player(name, elo));
+        }
+
+
+
 
         tm.startTournament();
+        input.close();
     }
 }
